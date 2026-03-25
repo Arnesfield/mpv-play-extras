140de5b193457a58404724c447e3d1d055ec0720
Jefferson Rylee <rylee.jeff385@gmail.com>
2026-03-25 19:24:44 +0800

    update autoplay, use start-file event instead

--- lib/autoplay.lua
+++ lib/autoplay.lua
@@ -1,14 +1,17 @@
--- automatically play when file is loaded
-mp.register_event('file-loaded', function()
+-- automatically play before file is loaded
+mp.register_event("start-file", function()
   -- start from the beginning if end of file or if the difference
   -- between the total video time and position is less than a second
-  local duration = mp.get_property_number('duration')
-  local time_pos = mp.get_property_number('time-pos')
-  local eof_reached = mp.get_property('eof-reached') == 'yes'
+  local duration = mp.get_property_number("duration")
+  local time_pos = mp.get_property_number("time-pos")
+  local eof_reached = mp.get_property("eof-reached") == "yes"
 
-  if eof_reached or (duration ~= nil and time_pos ~= nil and duration - time_pos < 1) then
-    mp.command('seek 0 absolute')
+  if
+    eof_reached
+    or (duration ~= nil and time_pos ~= nil and duration - time_pos < 1)
+  then
+    mp.command("seek 0 absolute")
   end
 
-  mp.command('set pause no')
+  mp.command("set pause no")
 end)
